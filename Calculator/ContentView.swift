b256fb83de0e67dc2d02028796bcf0a7fbfc44b0
Thomas Korsnes <89935350+Korsfareren@users.noreply.github.com>
2022-09-21 12:51:34 +0200

    Added all the things

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let symbols = ["C", "+/-", "%", "/", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "0", ",", "="]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                Text("0,135")
+                    .font(.system(size: 60))
+                    .foregroundColor(.white)
+            }
+            
+            LazyVGrid(columns: [GridItem(), GridItem(), GridItem(), GridItem()]) {
+                ForEach(symbols[0..<symbols.count], id: \.self ) { symbol in
+                    if symbol == "C" || symbol == "+/-" || symbol == "%" {
+                        ButtonView(symbol: symbol, symbolColor: Color.black, circleColor: Color.gray)
+                    } else if symbol == "/" || symbol == "x" || symbol == "-" || symbol == "+" || symbol == "=" {
+                        ButtonView(symbol: symbol, symbolColor: Color.white, circleColor: Color.orange)
+                    } else {
+                        ButtonView(symbol: symbol, symbolColor: Color.white, circleColor: Color(UIColor.darkGray))
+                    }
+                }
+            }
+        }
+        .background(.black)
     }
 }
 
@@ -19,3 +42,29 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct ButtonView: View {
+    let symbol: String
+    let symbolColor: Color
+    let circleColor: Color
+    
+    var body: some View {
+        ZStack {
+            if symbol == "0" {
+                Capsule()
+                    .frame(width: 140, height: 70)
+                    .foregroundColor(circleColor)
+            } else {
+                Circle()
+                    .frame(width: 70.0, height: 70.0, alignment: .center)
+                    .foregroundColor(circleColor)
+            }
+            Text(symbol)
+                .foregroundColor(symbolColor)
+                .font(.title)
+                .bold()
+            
+        }
+    }
+}
+
